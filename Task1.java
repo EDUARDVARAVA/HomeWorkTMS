036066f455c7fc3d83c4bde903ac84d73404915a
EDUARDVARAVA <ed.varava.com@mail.ru>
2022-09-29 20:59:51 +0300

    task1

---
+++ Task1.java
@@ -0,0 +1,27 @@
+public class Task1 {
+    public static void main(String[] args) {
+        switch ("7") {
+            case "12":
+            case "1":
+            case "2":
+                System.out.println("Зима");
+                break;
+            case "3":
+            case "4":
+            case "5":
+                System.out.println("Весна");
+                break;
+            case "6":
+            case "7":
+            case "8":
+                System.out.println("Лето");
+                break;
+            case "9":
+            case "10":
+            case "11":
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Введите число от 1 до 12");}
+    }
+}
