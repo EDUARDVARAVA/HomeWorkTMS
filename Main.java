e77729796afd29578836fe879a5392f3b6f33f55
EDUARDVARAVA <114606494+EDUARDVARAVA@users.noreply.github.com>
2022-09-29 20:28:26 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        switch ("1") {
+            case "1":
+                System.out.println("Весна");
+                break;
+            case "2":
+                System.out.println("Лето");
+                break;
+            case "3":
+                System.out.println("Осень");
+                break;
+            case "4":
+                System.out.println("Зима");
+                break;
+            default:
+                System.out.println("Нет совпадений");}
+        }
+    }
