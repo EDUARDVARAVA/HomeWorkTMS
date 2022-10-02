9ae51b92842d498146beb351d2f516624a24ead8
EDUARDVARAVA <ed.varava.com@mail.ru>
2022-10-02 17:57:25 +0300

    Task8

---
+++ Task8.java
@@ -0,0 +1,9 @@
+public class Task8 {
+    public static void main(String[] args) {
+        int x = 7;
+        while (x <= 98) {
+            System.out.print(x + " ");
+            x = x + 7;
+        }
+    }
+}
