162c95627f226e7b45d999e4c8502912cb20eb07
EDUARDVARAVA <ed.varava.com@mail.ru>
2022-10-02 19:42:27 +0300

    Task9

--- Task9.java
+++ Task9.java
@@ -1,7 +1,7 @@
 public class Task9 {
     public static void main(String[] args) {
         int x = 0;
-        System.out.print(x + "");
+        System.out.print(x);
         for (int i = 1; i < 10; i++) {
             x -= 5;
             System.out.print(" " + x);
