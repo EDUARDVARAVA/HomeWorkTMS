e69375ea7cf343f9216bc9f54c60de481e860e87
EDUARDVARAVA <ed.varava.com@mail.ru>
2022-10-02 19:58:44 +0300

    Task10

--- Task10.java
+++ Task10.java
@@ -1,3 +1,11 @@
 public class Task10 {
-
+    public static void main(String[] args) {
+        int x = 9;
+        while (x <= 20) {
+            x++;
+            if (x <= 20) {
+                System.out.println(Math.pow(x, 2));
+            }
+        }
+    }
 }
